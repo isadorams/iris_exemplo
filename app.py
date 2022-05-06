eb6fdb36325b4d09fbe190da8a0bda3e9a20b484
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-06 20:24:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+%%writefile app.py
 import streamlit as st
 
 st.write("# Classificação de Iris")
 st.write("## Exemplo com o comprimento de pétala e sépala")
+
+st.sidebar.write("### Parâmetros")
+st.sidebar.slider("Comprimento da sépala", 4, 8, 5.8, 0.1)
+st.sidebar.slider("Comprimento da pétala", 0.9, 7, 3.8, 0.1)
